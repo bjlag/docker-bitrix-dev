4fb538513f3de83d32310a851d55eedfec3bb045
bjlag <vlad.duplin@ya.ru>
2018-05-25 22:26:41 +0300

    небольшие улучшения

--- src/index.php
+++ src/index.php
@@ -1,3 +1,30 @@
 <?php
+if ( isset( $_GET[ 'phpinfo' ] ) && $_GET[ 'phpinfo' ] == 'y' ) {
+    phpinfo();
+    exit();
+}
+?>
 
-phpinfo();
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Docker Bitrix Dev</title>
+</head>
+<body>
+
+<h1>Docker Bitrix Dev</h1>
+<p>
+    <a href="/?phpinfo=y" target="_blank">PHP info</a>
+</p>
+<p>
+    Полезные скрипты:
+</p>
+<ul>
+    <li><a href="http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php">bitrixsetup.php</a></li>
+    <li><a href="http://www.1c-bitrix.ru/download/scripts/restore.php">restore.php</a></li>
+    <li><a href="http://dev.1c-bitrix.ru/download/scripts/bitrix_server_test.php">bitrix_server_test.php</a></li>
+</ul>
+
+</body>
+</html>
