9af0c1ac4d256ea58d3980464c8a796c008610d4
bjlag <vlad.duplin@ya.ru>
2018-06-06 22:26:52 +0300

    добавил возможность восстановления проекта

--- src/index.php
+++ src/index.php
@@ -1,7 +1,39 @@
 <?php
-if ( isset( $_GET[ 'phpinfo' ] ) && $_GET[ 'phpinfo' ] == 'y' ) {
-    phpinfo();
-    exit();
+
+ini_set( 'display_errors', 'off' );
+
+$error = false;
+$message = '';
+
+if ( isset( $_GET[ 'action' ] ) ) {
+    switch ( $_GET[ 'action' ] ) {
+        case 'phpinfo': {
+            phpinfo();
+            exit();
+            break;
+        }
+
+        case 'restore': {
+            $path = $_SERVER[ 'DOCUMENT_ROOT' ] . DIRECTORY_SEPARATOR . 'restore.php';
+            $url = 'http://www.1c-bitrix.ru/download/scripts/restore.php';
+
+            if ( !file_exists( $path ) ) {
+                $data = file_get_contents( $url );
+                if ( $data === false || file_put_contents( $path, $data ) === false ) {
+                    $error = true;
+                }
+            }
+
+            if ( !$error ) {
+                header( 'Location: http://' . $_SERVER[ 'HTTP_HOST' ] . '/restore.php' );
+                exit();
+            } else {
+                $message = 'Не удалось получить файл restore.php';
+            }
+
+            break;
+        }
+    }
 }
 ?>
 
@@ -14,8 +46,13 @@ if ( isset( $_GET[ 'phpinfo' ] ) && $_GET[ 'phpinfo' ] == 'y' ) {
 <body>
 
 <h1>Docker Bitrix Dev</h1>
+<div>
+    <?= $message ?>
+</div>
+
 <p>
-    <a href="/?phpinfo=y" target="_blank">PHP info</a>
+    <a href="/?action=phpinfo" target="_blank">PHP info</a><br>
+    <a href="/?action=restore">Восстановить 1С-Битрикс</a>
 </p>
 <p>
     Полезные скрипты:
